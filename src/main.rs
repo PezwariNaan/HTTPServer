3560081e320e3e8891159bf72f0b6e39a76931da
Pezwari_Naan <pezwarinaan@gmail.com>
2024-04-01 20:23:46 +0100

    Server doesn't exit after every request, Branch create to create logging

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,53 @@
-use std::{
-    fs,
-    net::{TcpListener, TcpStream},
-    io::{prelude::*, BufReader},
-    thread,
-    time::Duration,
-};
 use HTTPServer::ThreadPool;
-
-fn handle_connection(mut stream: TcpStream) {
-    let buffer_reader = BufReader::new(&mut stream);
-    let request_line = buffer_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = match &request_line[..]  {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
-    }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-    
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-    stream.write_all(response.as_bytes()).unwrap();
-}  
+use std::fs;
+use std::str;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9001").unwrap();
     let pool = ThreadPool::new(4);
-    
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
+        let ip = stream.peer_addr().unwrap();
+        println!("Client Addres: {}", ip);
+
         pool.execute(|| {
-            let ip = stream.peer_addr().unwrap();
-            println!("Connection Established\nClient: {}\n", ip);
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let number_of_bytes = stream.read(&mut buffer).unwrap();
+
+    // We can print the buffer - which holds the request sent from the client
+    // Next is to write it to file
+    //let request_details = str::from_utf8(&buffer[..number_of_bytes]);
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
