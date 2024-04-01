646c6e76b1577438fb42814581da60e676038746
Pezwari_Naan <pezwarinaan@gmail.com>
2024-04-01 16:40:54 +0100

    404 Page Created and Is Served By Server

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,22 @@ use std::{
 
 fn handle_connection(mut stream: TcpStream) {
     let buffer_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buffer_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request: {:#?}", http_request);
+    let request_line = buffer_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    println!("{}", request_line);
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-    
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    
+
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
     stream.write_all(response.as_bytes()).unwrap();
 }  
 
@@ -30,7 +32,7 @@ fn main() {
         let stream = stream.unwrap();
         let ip = stream.peer_addr().unwrap();
 
-        println!("Connection Established\nClient: {}", ip);
+        println!("Connection Established\nClient: {}\n", ip);
 
         handle_connection(stream);
     }
